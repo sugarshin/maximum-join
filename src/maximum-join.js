4a3805e8ac70addc42d6ed98ae3d3471beec3017
sugarshin <shinsugar@gmail.com>
2015-11-03 14:57:40 +0900

    refactor

--- src/maximum-join.js
+++ src/maximum-join.js
@@ -4,69 +4,64 @@
  * License: MIT
  */
 
-const toStr = 'toString';
+const toString = 'toString';
 
 export default function maximumJoin(array) {
   if (!Array.isArray(array)) {
-    throw new Error('Argument should be an Array.');
+    throw new TypeError('should be an Array.');
   }
 
-  let result = array.map(el => {
-                      if (typeof el !== 'number' ||
-                          isNaN(el) ||
-                          el === Infinity ||
-                          el === -Infinity) {
-                        return false;
-                      }
-                      return Math.abs(Math.floor(el));
-                    })
-                    .filter(el => {
-                      return el !== false;
-                    })
-                    .sort((a, b) => {
-                      if (a - b === 0) {
-                        return 0;
-                      }
+  let result = array.filter(el => (
+    typeof el === 'number' &&
+    !isNaN(el) &&
+    el !== Infinity &&
+    el !== -Infinity
+  ))
+  .map(n => Math.abs(Math.floor(n)))
+  .sort((a, b) => {
+    if (a - b === 0) {
+      return 0;
+    }
 
-                      let strA = a[toStr]();
-                      let strB = b[toStr]();
+    let strA = a[toString]();
+    let strB = b[toString]();
 
-                      let aLen = strA.length;
-                      let bLen = strB.length;
+    let aLen = strA.length;
+    let bLen = strB.length;
 
-                      let biggerLenNum, smallerLenNum, smallerLen;
+    let biggerLenNum, smallerLenNum, smallerLen;
 
-                      if (aLen > bLen) {
-                        biggerLenNum = strA;
-                        smallerLenNum = strB;
-                        smallerLen = bLen;
-                      } else if (aLen < bLen) {
-                        biggerLenNum = strB;
-                        smallerLenNum = strA;
-                        smallerLen = aLen;
-                      } else if (aLen === bLen) {
-                        smallerLen = aLen;
-                      }
+    if (aLen > bLen) {
+      biggerLenNum = strA;
+      smallerLenNum = strB;
+      smallerLen = bLen;
+    } else if (aLen < bLen) {
+      biggerLenNum = strB;
+      smallerLenNum = strA;
+      smallerLen = aLen;
+    } else if (aLen === bLen) {
+      smallerLen = aLen;
+    }
 
-                      for (let i = 0; i < smallerLen; i++) {
-                        let _b = +strB.slice(i, i + 1);
-                        let _a = +strA.slice(i, i + 1);
-                        if (_b !== _a) {
-                          return _b - _a;
-                        }
-                      }
+    for (let i = 0; i < smallerLen; i++) {
+      let _b = +strB.slice(i, i + 1);
+      let _a = +strA.slice(i, i + 1);
+      if (_b !== _a) {
+        return _b - _a;
+      }
+    }
 
-                      if (aLen === 1) {
-                        return -1;
-                      }
-                      if (bLen === 1) {
-                        return 1;
-                      }
+    if (aLen === 1) {
+      return -1;
+    }
+    if (bLen === 1) {
+      return 1;
+    }
 
-                      return +biggerLenNum.slice(smallerLen, smallerLen + 1) - +smallerLenNum.slice(0, 1);
+    return +biggerLenNum.slice(smallerLen, smallerLen + 1) - +smallerLenNum.slice(0, 1);
 
-                    })
-                    .join('');
+  })
+  .join('');
 
   return +result;
 }
